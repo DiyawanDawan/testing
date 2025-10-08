8c7f39e9a17aae032eb2f4c152f7090741ec8684
DiyawanDawan <diyawan0@gmail.com>
2025-10-08 16:45:27 +0800

    add

--- server.test.js
+++ server.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from './server.js';
+import app from './index.js';
 
 describe('Server Express Test', () => {
   test('GET / harus mengembalikan pesan sukses', async () => {
