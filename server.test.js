30c3ba446f272c09826ec14d9b6c4fe98f680fb2
DiyawanDawan <diyawan0@gmail.com>
2025-10-08 18:16:33 +0800

    tes

--- server.test.js
+++ server.test.js
@@ -5,6 +5,6 @@ describe('Server Express Test', () => {
   test('GET / harus mengembalikan pesan sukses', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('🚀 Express Docker Testing di localhost:3000');
+    expect(res.text).toContain('🚀 Express Docker Testing di');
   });
 });
